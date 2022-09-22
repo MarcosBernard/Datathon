69b6f277db90d2cff9eb214cbf47c80f643d440f
Marcos Bernard Calixto López <marcoscalixto92@gmail.com>
2022-09-22 00:06:25 -0500

    Modelo 1 con label encoder en todos los campos

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.svm import SVC
 from sklearn.metrics import recall_score
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
 
 
 #Carga de CSVs
 train_df = pd.read_csv("./Preprocesamiento/train_preproc.csv",sep=',',encoding='UTF-8')
 test_df = pd.read_csv("./Preprocesamiento/test_preproc.csv",sep=',',encoding='UTF-8')
 
-X = train_df.iloc[:,:-1]
-Y = train_df['Reached.on.Time_Y.N'].values
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.30, random_state=42) 
+#Escojemos los campos con mayor relevancia para entrenar
+#ver Correlación.ipynb
 
-clf = SVC(kernel='poly', random_state=42,degree=2,C=200)
-# Entrenamos
-clf.fit(X_train,y_train)
+entrenamiento = train_df.loc[:,['Discount_offered','Weight_in_gms','Prior_purchases']]
+
+# Instanciamos un objeto de la clase DecisionTreeClassifier
+clf = DecisionTreeClassifier(max_depth = 3, random_state = 42) 
+
+# Entrenamos el modelo
+clf.fit(entrenamiento.values,train_df['Reached.on.Time_Y.N'].values)
 # Predecimos para el dataset de entrenamiento
-y_pred = clf.predict(X_test)
+y_pred = clf.predict(entrenamiento.values)
 # Vemos la sensibilidad o recall.
-print('recall:',recall_score(y_test,y_pred))
-# Vemos la precisión.
-print('accuracy:',accuracy_score(y_test,y_pred))
-# Predecimos para la data test
-entrenamiento_test = test_df.iloc[:,:]
-y_pred_test = clf.predict(entrenamiento_test) #Predice valores para la subtabla pred_test
+print('recall:',recall_score(train_df['Reached.on.Time_Y.N'].values,y_pred))
+print('accuracy:',accuracy_score(train_df['Reached.on.Time_Y.N'].values,y_pred))
+# Predecimos
+entrenamiento_test = test_df.loc[:,['Discount_offered','Weight_in_gms','Prior_purchases']]
+y_pred_test = clf.predict(entrenamiento_test.values) #Predice valores para la subtabla pred_test
+
+
+
 #Exportamos
 salida = pd.DataFrame(y_pred_test,columns=['pred'])
 salida.to_csv('MarcosBernard.csv',index=False)
+
+print(salida.value_counts())
